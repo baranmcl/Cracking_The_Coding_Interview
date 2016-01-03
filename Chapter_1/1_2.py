411b2a0bd58a556f54404e0897ac304673be9890
baranmcl <baranmcl@gmail.com>
2016-01-03 17:37:26 -0500

    solved 1_2.py

--- Chapter_1/1_2.py
+++ Chapter_1/1_2.py
@@ -1,1 +1,7 @@
 ''' Implement a function void reverse(char* str) in C or C++ which reverses a null-terminated string. '''
+#I don't know C or C++ (yet) so here's my solution in Python.
+def stringReverse(n):
+    return ''.join(str(i) for i in n[-1::-1])
+    
+if __name__ == '__main__':
+    print(stringReverse('backwards!123@$-'))
