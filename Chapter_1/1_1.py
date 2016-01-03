2af87efa0612b0f0084572f47d943a55e0c9fee3
baranmcl <baranmcl@gmail.com>
2016-01-03 16:47:41 -0500

    created 1_1.py

---
+++ Chapter_1/1_1.py
@@ -0,0 +1,7 @@
+''' Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? '''
+def stringCheck(n):
+  return n
+  
+if __name__ == '__main__':
+  print(stringCheck('the quick brown fox'))
+  print(stringCheck('abcdefghijklmnop'))
