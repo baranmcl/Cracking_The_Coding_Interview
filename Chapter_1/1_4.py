7f1eb60d737d5e326883f38a8e184022aab2a042
baranmcl <baranmcl@gmail.com>
2016-01-03 18:08:58 -0500

    created 1_4.py

---
+++ Chapter_1/1_4.py
@@ -0,0 +1,9 @@
+''' 
+Write a method to replace all spaces in a string with '%20'. You may assume that the string has
+sufficient space at the end of the string to hold the additionanl characters, and that you are given
+"true" length of the string. (Note: if implementing in Java, please use a character array so taht you 
+can perform this operation in place.)
+EXAMPLE
+Input: "Mr John Smith    ", 13
+Output: "Mr%20John%20Smith"
+'''
