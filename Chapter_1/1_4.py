5fbe18325e9d3bae0f57dcaf94e18e6a112e4772
baranmcl <baranmcl@gmail.com>
2016-01-04 19:10:32 -0500

    solved 1_4.py

--- Chapter_1/1_4.py
+++ Chapter_1/1_4.py
@@ -7,3 +7,8 @@ EXAMPLE
 Input: "Mr John Smith    ", 13
 Output: "Mr%20John%20Smith"
 '''
+def spaceReplace(string, length):
+    return string[:13].replace(" ","%20")
+    
+if __name__ == '__main__':
+    print(spaceReplace("Mr John Smith    ", 13))
