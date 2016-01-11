8a5d967ad5f36fa7258cad2062dc7b4dfea3d288
baranmcl <baranmcl@gmail.com>
2016-01-10 22:54:07 -0500

    alternate solution
    solved without additional data structures.

--- Chapter_1/1_1.py
+++ Chapter_1/1_1.py
@@ -1,11 +1,28 @@
 ''' Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? '''
-def stringCheck(n):
-    characters = []
-    for i in n:
-        if i not in characters: characters.append(i)
-        else: return False
+
+#ask interviewer if string is ASCII or Unicode.
+
+#def stringCheck(n):
+#    characters = []
+#    for i in n:
+#        if i not in characters: characters.append(i)
+#        else: return False
+#    return True
+
+def bubbleSort(n):
+    for passnum in range(len(n)-1,0,-1):
+        for i in range(passnum):
+            if n[i]>n[i+1]:
+                n[i+1], n[i] = n[i], n[i+1]
+
+def stringCheck2(n):
+    if len(n) > 128: return False
+    sortedString = bubbleSort(n)
+    for letter in range(len(sortedString)):
+        if sortedString[letter] == sortedString[letter+1]:
+            return False
     return True
-  
+
 if __name__ == '__main__':
-    print(stringCheck('the quick brown fox'))
-    print(stringCheck('abcdefghijklmnop'))
+    print(stringCheck2('the quick brown fox'))
+    print(stringCheck2('abcdefghijklmnop'))
