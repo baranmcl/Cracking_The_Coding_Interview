204a8a23e42533c6446e295f2e74f53a36979fa2
baranmcl <baranmcl@gmail.com>
2016-01-12 18:31:27 -0500

    alternate solution
    removed second string hash. Hooray efficiency!

--- Chapter_1/1_3.py
+++ Chapter_1/1_3.py
@@ -1,20 +1,20 @@
 ''' Given two strings, write a method to decide if one is a permutation of the other. '''
 def permCheck(string1, string2):
     string1Hash = {}
-    string2Hash = {}
     for i in string1:
         if i not in string1Hash:
             string1Hash[i] = 1
         else:
             string1Hash[i] += 1
     for i in string2:
-        if i not in string2Hash:
-            string2Hash[i] = 1
+        if i in string1Hash:
+            string1Hash[i] -= 1
         else:
-            string2Hash[i] += 1
-    if string1Hash == string2Hash:
-        return True
-    return False
+            return False
+    for i in string1Hash:
+        if string1Hash[i] != 0:
+            return False
+    return True
     
 if __name__ == '__main__':
     print(permCheck('taxis', 'satix'))
