eec44c53d6d57239c0591f8c85a16adfbb159a48
baranmcl <baranmcl@gmail.com>
2016-01-18 16:54:30 -0500

    actually got a list working

--- Chapter_2/linkedlist.py
+++ Chapter_2/linkedlist.py
@@ -15,8 +15,20 @@ class Node:
 class LinkedList:
   def __init__(self, head_node=None):
     self.head_node = head_node
-  def displayList(self, head_node):
-    current = head_node
+    self.tail_node = head_node
+  def insert(self, data):
+    new_node = Node(data)
+    self.tail_node.set_next(new_node)
+    self.tail_node = new_node
+  def displayList(self):
+    current = self.head_node
     while current:
       print(current.get_data())
       current = current.get_next()
+      
+      
+linkeddata = [5,4,1,2,3]
+newList = LinkedList(Node(linkeddata[0]))
+for i in linkeddata[1:]:
+    newList.insert(i)
+newList.displayList()
