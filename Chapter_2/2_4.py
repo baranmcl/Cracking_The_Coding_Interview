7685e5382a0cef02d386f8647271b40345464b47
baranmcl <baranmcl@gmail.com>
2016-01-22 22:30:25 -0500

    solved problem 2_4

--- Chapter_2/2_4.py
+++ Chapter_2/2_4.py
@@ -2,3 +2,78 @@
 Write code to partition a linked list around a value x, 
 such that all nodes less than x come before all nodes greater than or equal to x.
 '''
+class Node:
+  def __init__(self, data, next_node=None):
+    self.data = data
+    self.next_node = next_node
+  def set_next(self, node):
+    self.next_node = node
+  def get_next(self):
+    return self.next_node
+  def set_data(self, data):
+    self.data = data
+  def get_data(self):
+    return self.data
+
+class LinkedList:
+  def __init__(self, head_node=None):
+    self.head_node = head_node
+    self.tail_node = head_node
+  def insert(self, data):
+    new_node = Node(data)
+    if self.head_node == None:
+        self.head_node = self.tail_node = new_node
+    else:
+        self.tail_node.set_next(new_node)
+        self.tail_node = new_node
+  def display(self):
+    current = self.head_node
+    while current:
+      print(current.get_data())
+      current = current.get_next()
+  def displayList(self):
+    current = self.head_node
+    result = []
+    while current:
+        result.append(current.get_data())
+        current = current.get_next()
+    return result
+  def removeByData(self, data):
+    current = self.head_node
+    previous = None
+    while current:
+        if current.get_data() == data:
+            if previous != None:
+                previous.set_next(current.get_next())
+                break
+            else:
+                current = current.get_next()
+        previous = current
+        current = current.get_next()
+  def removeNode(self, data):
+      currentNode = node
+      nextNode = node.get_next()
+      currentNode.set_data(nextNode.get_data())
+      currentNode.set_next(nextNode.get_next())
+  def partitionList(self, data):
+      current = self.head_node
+      smaller = LinkedList()
+      larger = LinkedList()
+      while current:
+          node_data = current.get_data()
+          if node_data < data:
+              smaller.insert(node_data)
+          elif node_data >= data:
+              larger.insert(node_data)
+          current = current.get_next()
+      smaller.tail_node.set_next(larger.head_node)
+      return smaller
+
+if __name__ == "__main__":
+    linkeddata = [5,4,1,2,3,4,6,5,2]
+    newList = LinkedList(Node(linkeddata[0]))
+    for i in linkeddata[1:]:
+        newList.insert(i)
+    print(newList.displayList())
+    newList = newList.partitionList(3)
+    print(newList.displayList())
