3d94aa81f0333536695ba8e6a0b834b69542df06
baranmcl <baranmcl@gmail.com>
2016-01-24 11:58:54 -0500

    solved 2_7.py

--- Chapter_2/2_7.py
+++ Chapter_2/2_7.py
@@ -1,3 +1,49 @@
 '''
 Implement a function to check if a linked list is a palindrome.
 '''
+class Node:
+  def __init__(self, data, next_node=None):
+    self.data = data
+    self.next_node = next_node
+  def set_next(self, node):
+    self.next_node = node
+  def get_next(self):
+    return self.next_node
+  def set_data(self, data):
+    self.data = data
+  def get_data(self):
+    return self.data
+
+class LinkedList:
+  def __init__(self, head_node=None):
+    self.head_node = head_node
+    self.tail_node = head_node
+  def insert(self, data):
+    new_node = Node(data)
+    self.tail_node.set_next(new_node)
+    self.tail_node = new_node
+  def display(self):
+    current = self.head_node
+    while current:
+      print(current.get_data())
+      current = current.get_next()
+  def displayList(self):
+    current = self.head_node
+    result = []
+    while current:
+        result.append(current.get_data())
+        current = current.get_next()
+    return result
+def isPalindrome(LList):
+  data = LList.displayList()
+  if data == data[:-1]: return True
+  else: return False
+
+
+if __name__ == "__main__":
+    linkeddata = [5,4,1,2,3,4,6,5,2]
+    newList = LinkedList(Node(linkeddata[0]))
+    for i in linkeddata[1:]:
+        newList.insert(i)
+    print(newList.displayList())
+    print(isPalindrome(newList))
