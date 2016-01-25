bbc19d3fad1fe6b0ed7ab850c5befd5e46eaf0a8
baranmcl <baranmcl@gmail.com>
2016-01-24 22:00:18 -0500

    created file

---
+++ Chapter_3/3_7.py
@@ -0,0 +1,8 @@
+'''
+An animal shelter holds only dogs and cats, and operates on a strictly "first in, first out" basis. 
+People must adopt either the "oldest" (based on arrival time) if all animals at the shelter, 
+or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of that type).
+They cannot select which specific animal they would like.
+Create the data structure to maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog and dequeueCat.
+You may use teh built-in LinkedList data structure.
+'''
