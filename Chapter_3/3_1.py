5ee269467b680ad6053d700816faf810ae8abd04
baranmcl <baranmcl@gmail.com>
2016-01-25 22:48:18 -0500

    partially answered one

--- Chapter_3/3_1.py
+++ Chapter_3/3_1.py
@@ -1,3 +1,7 @@
 '''
 Describe how you could use a single array to implement three stacks.
 '''
+'''
+1) Split the array into three even sections, (1, n/3), (n/3, 2n/3), & (2n/3, n), and implement the stacks.
+2) ?
+'''
