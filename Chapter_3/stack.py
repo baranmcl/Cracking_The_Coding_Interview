d191505d72f465ad1a12519d67c65942fbbd9b73
baranmcl <baranmcl@gmail.com>
2016-01-25 22:56:32 -0500

    built stack class

--- Chapter_3/stack.py
+++ Chapter_3/stack.py
@@ -1,3 +1,16 @@
 '''
 I'm going to implement an instance of a stack for testing in python.
+LIFO!!!
 '''
+class Stack:
+  def __init__(self):
+    self.items = []
+  def isEmpty(self):
+    return self.items == []
+  def push(self, item):
+    self.items.append(item)
+  def pop(self, item):
+    self.items.pop()
+  def size(self):
+    return len(self.items)
+
