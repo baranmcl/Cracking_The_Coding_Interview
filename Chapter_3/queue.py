157b83f42598ca19020f9a9bd71a86d2067afe8c
baranmcl <baranmcl@gmail.com>
2016-01-25 22:59:18 -0500

    built queue class

--- Chapter_3/queue.py
+++ Chapter_3/queue.py
@@ -2,3 +2,14 @@
 I'm going to implement a queue data structure for me to mess around with.
 FIFO!
 '''
+class Queue:
+  def __init__(self):
+    self.items = []
+  def isEmpty(self):
+    return self.items == []
+  def push(self, item):
+    self.items.append(item)
+  def pop(self, item):
+    self.items.pop(0)
+  def size(self):
+    return len(self.items)
