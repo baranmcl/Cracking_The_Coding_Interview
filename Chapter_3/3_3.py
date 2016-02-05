52dbe91fd7d48f33dcc0c523c52cc484b3c1a41f
baranmcl <baranmcl@gmail.com>
2016-02-05 00:17:47 -0500

    started problem

--- Chapter_3/3_3.py
+++ Chapter_3/3_3.py
@@ -8,3 +8,31 @@ SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single s
 FOLLOW UP
 Implement a function popAt(int index) which performs a pop operation on a specific sub-stack.
 '''
+'''
+Imagine a (literal) stack of plates. If the stack gets too high, it might topple. 
+Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold.
+Implement a data structure SetOf Stacks that mimics this.
+SetOfStacks should be composed of several stacks and should create a new stack once the previous one exceeds capacity.
+SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack
+(that is, pop() should return the same values as it would if ther ewere just a single stack).
+FOLLOW UP
+Implement a function popAt(int index) which performs a pop operation on a specific sub-stack.
+'''
+class Stack:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self, item):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+
+class SetofStacks:
+    def __init__(self, limit):
+        self.stacks = []
+        self.limit = limit
+    def push(self, stack):
+        if stack.size()
