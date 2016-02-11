36a8ee6aa845013b8fc85827ae44e1c81e68611e
baranmcl <baranmcl@gmail.com>
2016-02-10 23:19:52 -0500

    solved 3_4.py

--- Chapter_3/3_4.py
+++ Chapter_3/3_4.py
@@ -7,3 +7,42 @@ The puzzle starts with disks sorted in ascending order of size from top to botto
 (3) A disk can only be placed on top of a larger disk.
 Write a program to move the disks from the first tower to the last using stacks.
 '''
+class Stack:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+    def listItems(self):
+        print(self.items)
+        
+def hanoi(n, source, helper, target):
+    if n>0:
+        # move tower of size n - 1 to helper
+        hanoi(n-1, source, target, helper)
+        # move from source peg to target
+        if source:
+            target.push(source.pop())
+        # move tower of size n - 1 from helper to target
+        hanoi(n-1, helper, source, target)
+        
+tower1 = Stack()
+tower2 = Stack()
+tower3 = Stack()
+tower1.push(4)
+tower1.push(3)
+tower1.push(2)
+tower1.push(1)
+tower1.listItems()
+tower2.listItems()
+tower3.listItems()
+print('---')
+hanoi(tower1.size(), tower1, tower2, tower3)
+tower1.listItems()
+tower2.listItems()
+tower3.listItems()
