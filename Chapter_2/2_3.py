9181c22f507288b5597876044478af39fd06d7c7
baranmcl <baranmcl@gmail.com>
2016-02-15 12:11:14 -0500

    updated solution
    includes proper pythonic attribute properties

--- Chapter_2/2_3.py
+++ Chapter_2/2_3.py
@@ -8,14 +8,20 @@ class Node:
   def __init__(self, data, next_node=None):
     self.data = data
     self.next_node = next_node
-  def set_next(self, node):
+    
+  @property
+  def next_n(self):
+    return self.next_node    
+  @next_n.setter
+  def next_n(self, node):
     self.next_node = node
-  def get_next(self):
-    return self.next_node
-  def set_data(self, data):
-    self.data = data
-  def get_data(self):
+  
+  @property  
+  def _data(self):
     return self.data
+  @_data.setter
+  def _data(self, data):
+    self.data = data
 
 class LinkedList:
   def __init__(self, head_node=None):
@@ -23,47 +29,47 @@ class LinkedList:
     self.tail_node = head_node
   def insert(self, data):
     new_node = Node(data)
-    self.tail_node.set_next(new_node)
+    self.tail_node.next_n = new_node
     self.tail_node = new_node
   def display(self):
     current = self.head_node
     while current:
-      print(current.get_data())
-      current = current.get_next()
+      print(current._data)
+      current = current.next_n
   def displayList(self):
     current = self.head_node
     result = []
     while current:
-        result.append(current.get_data())
-        current = current.get_next()
+        result.append(current._data)
+        current = current.next_n
     return result
   def displayList2(self):
     current = self.head_node
     result = []
     while current:
-        if current.get_data() == 3:
+        if current._data == 3:
             self.removeNode(current)
         else:
-            result.append(current.get_data())
-            current = current.get_next()
+            result.append(current._data)
+            current = current.next_n
     return result
   def removeByData(self, data):
     current = self.head_node
     previous = None
     while current:
-        if current.get_data() == data:
+        if current._data == data:
             if previous != None:
-                previous.set_next(current.get_next())
+                previous.next_n = current.next_n
                 break
             else:
-                current = current.get_next()
+                current = current.next_n
         previous = current
-        current = current.get_next()
+        current = current.next_n
   def removeNode(self, node):
       currentNode = node
-      nextNode = node.get_next()
-      currentNode.set_data(nextNode.get_data())
-      currentNode.set_next(nextNode.get_next())
+      nextNode = node.next_n
+      currentNode._data = nextNode._data
+      currentNode.next_n = nextNode.next_n
 
 if __name__ == "__main__":
     linkeddata = [5,4,1,2,3,4,6,5,2]
