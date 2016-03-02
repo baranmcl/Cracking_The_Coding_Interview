16be5e78581e441dd82143d2ba3279c8d38877e4
baranmcl <baranmcl@gmail.com>
2016-03-02 18:56:10 -0500

    solved 4_2.py

--- Chapter_4/4_2.py
+++ Chapter_4/4_2.py
@@ -1,3 +1,23 @@
 '''
 Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
 '''
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath: return newpath
+    return None
+
+graph = {'A': ['B', 'C'],
+         'B': ['C', 'D'],
+         'C': ['D'],
+         'D': ['C'],
+         'E': ['F'],
+         'F': ['C']}
+
+print(find_path(graph, 'A', 'D'))
