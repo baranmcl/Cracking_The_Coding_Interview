90383c334727f2de0fd7666bed95274b75d6a97c
baranmcl <baranmcl@gmail.com>
2016-03-03 23:18:27 -0500

    solved 4_3.py

--- Chapter_4/4_3.py
+++ Chapter_4/4_3.py
@@ -1,4 +1,33 @@
 '''
-Given a sorted (increasing order array with unique integer elements, 
+Given a sorted increasing order array with unique integer elements, 
 write an algorithm to create a binary search tree with minimal height.
 '''
+class tNode:
+    def __init__(self, data=None, L=None, R=None):
+        self.data = data
+        self.L = L
+        self.R = R
+
+def sortedArraytoBSTRecursive(num, begin, end):
+    if begin > end:
+        return None
+    midPoint = (begin+end) / 2
+    root = tNode(num[midPoint])
+    root.L = sortedArraytoBSTRecursive(num, begin, midPoint - 1)
+    root.R = sortedArraytoBSTRecursive(num, midPoint+1, end)
+    return root
+
+def sortedArraytoBST(num):
+    return sortedArraytoBSTRecursive(num, 0, len(num)-1)
+
+array = [1,2,3,4,5,6,7,8,9]
+myNode = sortedArraytoBST(array)
+print(myNode.data)
+print(myNode.L.data)
+print(myNode.L.L.data)
+print(myNode.L.R.data)
+print(myNode.L.R.R.data)
+print(myNode.R.data)
+print(myNode.R.L.data)
+print(myNode.R.R.data)
+print(myNode.R.R.R.data)
