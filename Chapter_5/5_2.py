e116d989de84f2649d5194c2e7b3cafcd4d4d915
baranmcl <baranmcl@gmail.com>
2016-03-16 19:27:31 -0400

    created 5_2.py

---
+++ Chapter_5/5_2.py
@@ -0,0 +1,4 @@
+'''
+Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a double, print the binary representation. 
+If th enumber cannot be represented accurately in binary with at most 32 characters, print "ERROR."
+'''
