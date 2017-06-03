86740a9d77c04c1417a645dd6e7acca376ff44fb
baranmcl <baranmcl@gmail.com>
2017-06-02 21:57:52 -0400

    problem not solved
    there's a bug with getting exponents of five

--- Chapter_17/17_3.py
+++ Chapter_17/17_3.py
@@ -1,1 +1,24 @@
 #Write an algorithm which computes the number of trailing zeros in n factorial.
+#Write an algorithm which computes the number of trailing zeros in n factorial.
+def zeroCountFactorial(n):
+  zeroCount = 0
+  while n > 0:
+    if (n % 5 == 0):
+      i = n
+      while i > 5:
+        if (i / 5 == 5):
+          zeroCount += 1
+          i = i / 5
+      zeroCount += 1
+    n -= 1
+  return zeroCount
+  
+def factorial(n):
+  ans = 1
+  while n > 0:
+    ans *= n
+    n -= 1
+  return ans
+
+print(factorial(25))
+print(zeroCountFactorial(25))
