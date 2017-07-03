dce3f824d09f2ab2bba7911d33cf1b504116ddfa
baranmcl <baranmcl@gmail.com>
2017-07-03 17:57:16 -0400

    solved problem

--- Chapter_17/17_5.py
+++ Chapter_17/17_5.py
@@ -8,3 +8,37 @@ Note that a slot that is a hit can never count as a pseudo-hit.
 For example, if the actual solution is RGBY and you guess GGRR, you have one hit and one pseudo-hit.
 Write a method that, given a guess and a solution, returns the number of hits and pseudo-hits.
 '''
+solution = 'RGBY'
+guess = input('Enter a guess: ')
+
+def calcPseudoHits(solution, guess, hits):
+  #Get counts of correct colors
+  solutionDict = {"R":0, "G":0, "B":0, "Y":0}
+  guessDict = {"R":0, "G":0, "B":0, "Y":0}
+  for i in solution:
+    solutionDict[i] += 1
+  for i in guess:
+    guessDict[i] += 1
+  #Find possible hits and subtract actual hits
+  pseudoHits = 0
+  for i in solutionDict:
+    if solutionDict[i] == guessDict[i]:
+      pseudoHits += solutionDict[i]
+  if pseudoHits - hits < 0:
+    return 0
+  return pseudoHits - hits
+
+def mastermind(solution, guess):
+  hits = 0
+  pseudoHits = 0
+  for i in range(4):
+    if solution[i] == guess[i]:
+      hits += 1
+  pseudoHits = calcPseudoHits(solution, guess, hits)
+  print("Hits: " + str(hits))
+  print("Pseudo-Hits: " + str(pseudoHits))
+  if solution == guess:
+    return "You Win!"
+  return "Try Again!"
+
+print(mastermind(solution, guess))
