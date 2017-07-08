c5989899634d9f11d117cdcfdf814292e5b46af1
baranmcl <baranmcl@gmail.com>
2017-07-08 15:57:38 -0400

    added prompt

---
+++ Chapter_17/17_8.py
@@ -0,0 +1,7 @@
+'''
+You are given an array of numbers (both positive and negative). Find the continuous sequence with the largest sum.
+Return the sum.
+EXAMPLE
+Input: 2, -8, 3, -2, 4, -10
+Output: 5 (i.e., {3, -2, 4})
+'''
