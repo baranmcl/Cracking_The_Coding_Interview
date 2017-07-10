531b7cb3fd19b514b70f7db6f651982f0aa1331c
baranmcl <baranmcl@gmail.com>
2017-07-10 17:07:43 -0400

    partially solved problem
    works up to thousands

--- Chapter_17/17_7.py
+++ Chapter_17/17_7.py
@@ -2,29 +2,36 @@
 Given any integer, print an English phrase that describes the integer
 (e.g. "One Thousand, Two Hundred Eighty Four").
 '''
-def intConvert(n):
-  num = n
-  ones = { '1':'one', '2':'two', '3':'three', '4':'four', '5':'five', '6':'six', '7':'seven', '8':'eight', '9':'nine', '0':'zero'}
-  teens = { '11':'eleven', '12':'twelve', '13':'thirteen', '14':'fourteen', '15':'fifteen', '16':'sixteen', '17':'seventeen', '18':'eighteen', '19':'nineteen'}
-  tens = { '1':'ten', '2':'twenty', '3':'thirty', '4':'forty', '5':'fifty', '6':'sixty', '7':'seventy', '8':'eighty', '9':'ninety'}
-  bigs = ['hundred', 'thousand', 'million', 'billion', 'trillion']
+def intConvert(num):
   
-  answer = ""
-  #Convert 100's
-  if num >= 100:
-    answer += ones[str(num)[0]] + ' hundred'
-    num %= 100
-  #Convert 10's
-  if (num >= 20):
-    answer += ' ' + tens[str(num)[0]]
-    num %= 10
-  #Convert teens
-  if (num >= 11) and (num <= 19):
-    answer += ' and ' + teens[str(num)]
-  #Convert 1's
-  if (num >= 1) and (num <=9):
-    answer += ' ' + ones[str(num)]
+  def hundredsConvert(num):
+    ones = { '1':'one', '2':'two', '3':'three', '4':'four', '5':'five', '6':'six', '7':'seven', '8':'eight', '9':'nine', '0':'zero'}
+    teens = { '11':'eleven', '12':'twelve', '13':'thirteen', '14':'fourteen', '15':'fifteen', '16':'sixteen', '17':'seventeen', '18':'eighteen', '19':'nineteen'}
+    tens = { '1':'ten', '2':'twenty', '3':'thirty', '4':'forty', '5':'fifty', '6':'sixty', '7':'seventy', '8':'eighty', '9':'ninety'}
+    bigs = ['hundred', 'thousand', 'million', 'billion', 'trillion']
+  
+    answer = ""
+    #Convert 100's
+    if num >= 100:
+      answer += ones[str(num)[0]] + ' hundred'
+      num %= 100
+    #Convert 10's
+    if (num >= 20):
+      answer += ' ' + tens[str(num)[0]]
+      num %= 10
+    #Convert teens
+    if (num >= 11) and (num <= 19):
+      answer += ' and ' + teens[str(num)]
+    #Convert 1's
+    if (num >= 1) and (num <=9):
+      answer += ' ' + ones[str(num)]
+    return answer
     
-  return answer
+  ans = ""
+  #Convert 1000's
+  if num >= 1000 and num <= 999999:
+    ans += hundredsConvert(int(str(num)[:-3:])) + " thousand, " + hundredsConvert(int(str(num)[-3::]))
+    
+  return ans
 
-print(intConvert(541))
+print(intConvert(234850))
