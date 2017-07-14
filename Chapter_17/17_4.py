c3e5ed064efd2e3a2c00a67f05a7c42ffce2c1c8
baranmcl <baranmcl@gmail.com>
2017-07-13 23:08:21 -0400

    solved problem

--- Chapter_17/17_4.py
+++ Chapter_17/17_4.py
@@ -1,1 +1,7 @@
 #Write a method which finds the maximum of two numbers. You should not use if-else or any other comparison operator. 
+import math
+
+def maximum(a, b):
+  return (math.sqrt((a*a) + (b*b) - (2*a*b)) + a + b) / 2
+  
+print(maximum(-10, 4))
