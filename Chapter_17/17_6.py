33ef2eace3338bff9902704563e64de7457ef7ff
baranmcl <baranmcl@gmail.com>
2017-07-16 14:42:40 -0400

    attempted solution

--- Chapter_17/17_6.py
+++ Chapter_17/17_6.py
@@ -6,3 +6,15 @@ EXAMPLE
 Input: 1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19
 Output: (3, 9)
 '''
+arr = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
+
+def solution(a):
+  n = a[0]
+  m = a[0]
+  for i in range(len(a)):
+    if n <= a[i+1]:
+      n = a[i]
+      print(a[i])
+  return a
+  
+print(solution(arr))
