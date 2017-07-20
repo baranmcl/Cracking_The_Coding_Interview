f837cd0b33ec86120e1a3333de138d1a0134a5bb
baranmcl <baranmcl@gmail.com>
2017-07-20 15:24:28 -0400

    outlined solution

--- Chapter_17/17_6.py
+++ Chapter_17/17_6.py
@@ -9,12 +9,22 @@ Output: (3, 9)
 arr = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
 
 def solution(a):
-  n = a[0]
-  m = a[0]
-  for i in range(len(a)):
-    if n <= a[i+1]:
-      n = a[i]
-      print(a[i])
-  return a
-  
+  #scan left to right to find first value out of order
+  scanLtoR = 0
+  scanLtoRIndex = 0
+  for i in a:
+    if a[i] < a[i+1]:
+      scanLtoR = a[i+1]
+      scanLtoRIndex = i+1
+    else:
+      break
+  print(scanLtoR, scanLtoRIndex)
+  #scan right to left to find first value out of order
+  scanRtoL = 0
+  scanRtoLIndex = 0
+  #INSERT SCANNING CODE HERE
+  #scan through the min and the max indices, and test if values are outside min and max values
+  #if outside, expand min and max by one and repeat previous step
+  #return min and max indices
+
 print(solution(arr))
