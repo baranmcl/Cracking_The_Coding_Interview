0bf67021fc9c07bdefd174a8a8b2c6e3571d6349
baranmcl <baranmcl@gmail.com>
2017-07-23 21:09:37 -0400

    work in progress

--- Chapter_17/17_8.py
+++ Chapter_17/17_8.py
@@ -5,3 +5,17 @@ EXAMPLE
 Input: 2, -8, 3, -2, 4, -10
 Output: 5 (i.e., {3, -2, 4})
 '''
+Input = [2, -8, 3, -2, 4, -10]
+
+def greatestSum(series):
+    answer = 0
+    for i in range(0,len(series)):
+        total = 0
+        for j in range(i+1):
+            total += series[j]
+        print(str(total) + '!')
+        if total > answer:
+            answer = total
+    return answer
+    
+print(greatestSum(Input))
