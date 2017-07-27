c484432d850b293bc1073c0263e9ad6863346266
baranmcl <baranmcl@gmail.com>
2017-07-27 13:02:34 -0400

    simple solution implemented

--- Chapter_17/17_9.py
+++ Chapter_17/17_9.py
@@ -1,3 +1,14 @@
 '''
 Design a method to find the frequency of occurences of any given word in a book.
 '''
+bookText = ("You see how many people are selling it, and crucially, you see how many people are buying it for what price.")
+
+def wordCount(word, text):
+  wordArray = text.strip().split(" ")
+  count = 0
+  for i in wordArray:
+    if word == i:
+      count += 1
+  return count
+  
+print(wordCount("you", bookText))
