1db5da95b5510cb8f00779fc5849d03723f727b2
baranmcl <baranmcl@gmail.com>
2017-08-02 23:59:00 -0400

    added algorithm outline

--- Chapter_17/17_12.py
+++ Chapter_17/17_12.py
@@ -1,3 +1,11 @@
 '''
 Design an algorithm to find all pairs of integers within an array which sum to a specified value.
 '''
+#if the array is sorted
+#binary search for number that adds to array number to get desired sum
+
+
+#if the array is unsorted
+#sort the array
+
+#do a binary search
