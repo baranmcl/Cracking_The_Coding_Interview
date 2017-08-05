4b65694a291208e14cfd6998da0c7349897ddb96
baranmcl <baranmcl@gmail.com>
2017-08-04 20:23:48 -0400

    almost solved problem

--- Chapter_17/17_6.py
+++ Chapter_17/17_6.py
@@ -10,21 +10,34 @@ arr = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
 
 def solution(a):
   #scan left to right to find first value out of order
-  scanLtoR = 0
-  scanLtoRIndex = 0
-  for i in a:
-    if a[i] < a[i+1]:
-      scanLtoR = a[i+1]
-      scanLtoRIndex = i+1
-    else:
+  scan_L_to_R = 0
+  scan_L_to_R_Index = 0
+  for i in range(len(a)):
+    if a[i] > a[i+1]:
+      scan_L_to_R = a[i]
+      scan_L_to_R_Index = i
       break
-  print(scanLtoR, scanLtoRIndex)
+  if (scan_L_to_R_Index == len(a)):
+    print("the array is sorted")
+    return
+  print(scan_L_to_R, scan_L_to_R_Index)
   #scan right to left to find first value out of order
-  scanRtoL = 0
-  scanRtoLIndex = 0
-  #INSERT SCANNING CODE HERE
+  scan_R_to_L = 0
+  scan_R_to_L_Index = 0
+  for i in reversed(range(len(a))):
+    if a[i] < a[i-1]:
+      scan_R_to_L = a[i]
+      scan_R_to_L_Index = i
+      break
+  print(scan_R_to_L, scan_R_to_L_Index)
   #scan through the min and the max indices, and test if values are outside min and max values
+  left_arr_max = a[scan_L_to_R_Index]
+  right_arr_min = a[scan_R_to_L_Index]
   #if outside, expand min and max by one and repeat previous step
+  while max(a[:scan_R_to_L_Index]) > right_arr_min:
+    scan_R_to_L_Index -= 1
+  while min(a[scan_L_to_R_Index:]) < left_arr_max:
+    scan_L_to_R_Index += 1
   #return min and max indices
-
+  return scan_R_to_L_Index, scan_L_to_R_Index
 print(solution(arr))
