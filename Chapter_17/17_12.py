56d29a9fb02f087ebda0d3481d18af1d3d0032c5
baranmcl <baranmcl@gmail.com>
2017-08-11 11:43:40 -0400

    solved scenarios
    may need further testing. how does it handle duplicates? negatives?

--- Chapter_17/17_12.py
+++ Chapter_17/17_12.py
@@ -4,28 +4,28 @@ Design an algorithm to find all pairs of integers within an array which sum to a
 #if the array is sorted
 #binary search for number that adds to array number to get desired sum
 a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-def binarySearch(alist, item):
+def binarySearch(a_list, item):
   first = 0
-  last = len(alist)-1
+  last = len(a_list)-1
   found = False
   
-  while first<=last and not found:
+  while first <= last and not found:
     midpoint = (first + last)//2
-    if alist[midpoint] == item:
+    if a_list[midpoint] == item:
       found = True
     else:
-      if item < alist[midpoint]:
+      if item < a_list[midpoint]:
         last = midpoint-1
       else:
         first = midpoint+1
   return found
 
-def pairCount(alist, desired_total):
+def pairCount(a_list, desired_total):
   count = 0
   first = 0
-  while first < len(alist):
-    target = desired_total - alist[first]
-    if binarySearch(alist, target):
+  while first < len(a_list):
+    target = desired_total - a_list[first]
+    if binarySearch(a_list, target):
       count += 1
     first += 1
   return int(count/2)
@@ -36,5 +36,18 @@ print(pairCount(a1, 14))
 #sort the array
 a2 = [2, 4, 7, 6, 5, 1, 3, 9, 10, 8]
 
-
+def insertionSort(a_list):
+  for index in range(1, len(a_list)):
+    currentvalue = a_list[index]
+    position = index
+    
+    while position > 0 and a_list[position-1]>currentvalue:
+      a_list[position] = a_list[position-1]
+      position = position-1
+      
+    a_list[position] = currentvalue
+  return a_list
+  
+print(insertionSort(a2))
 #do a binary search
+print(pairCount(insertionSort(a2), 14))
